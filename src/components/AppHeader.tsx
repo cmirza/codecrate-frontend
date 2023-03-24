88d16eaf2cb752eba089801dc72128b7a83d162a
cmirza <mail@cmirza.com>
2023-03-24 16:09:53 -0700

    Initial commit

---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppHeader: React.FC = () => {
+  return (
+    <header className="app-header">
+      <h1>CodeCrate</h1>
+    </header>
+  );
+};
+
+export default AppHeader;
