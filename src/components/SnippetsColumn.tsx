4042b83c844a6726567fa6f7214ffdd72f581b6c
cmirza <mail@cmirza.com>
2023-03-31 17:14:07 -0700

    added snippet deletion functionality

--- src/components/SnippetsColumn.tsx
+++ src/components/SnippetsColumn.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SnippetsColumn.css';
 
 interface SnippetsColumnProps {
@@ -7,24 +7,66 @@ interface SnippetsColumnProps {
 }
 
 const SnippetsColumn: React.FC<SnippetsColumnProps> = ({ selectedCollection, setSelectedSnippet }) => {
-  const snippets: Record<string, string[]> = {
+  const [snippets, setSnippets] = useState<Record<string, string[]>>({
     'Collection 1': ['Snippet 1-1', 'Snippet 1-2', 'Snippet 1-3', 'Snippet 1-4'],
     'Collection 2': ['Snippet 2-1', 'Snippet 2-2', 'Snippet 2-3', 'Snippet 2-4'],
     'Collection 3': ['Snippet 3-1', 'Snippet 3-2', 'Snippet 3-3', 'Snippet 3-4'],
     'Collection 4': ['Snippet 4-1', 'Snippet 4-2', 'Snippet 4-3', 'Snippet 4-4']
-  };
+  });
 
   const displayedSnippets = selectedCollection ? snippets[selectedCollection] : [];
+  const [newSnippetName, setNewSnippetName] = useState<string>('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewSnippetName(event.target.value);
+  };
+
+  const handleDeleteSnippet = (snippetToDelete: string) => {
+    if (selectedCollection) {
+      const updatedSnippets = {
+        ...snippets,
+        [selectedCollection]: snippets[selectedCollection].filter(
+          snippet => snippet !== snippetToDelete
+          )
+      };
+      setSnippets(updatedSnippets);
+      setSelectedSnippet(null);
+    }
+  }
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (newSnippetName.trim() && selectedCollection) {
+      const updatedSnippets = {
+        ...snippets,
+        [selectedCollection]: [...snippets[selectedCollection], newSnippetName.trim()]
+      };
+      setSnippets(updatedSnippets);
+      setNewSnippetName('');
+    }
+  };
 
   return (
     <div className="snippets-column">
       <ul>
         {displayedSnippets.map((snippet, index) => (
-          <li key={index} onClick={() => setSelectedSnippet(snippet)}>
-            {snippet}
+          <li key={index}>
+            <span onClick={() => setSelectedSnippet(snippet)}>{snippet}</span>
+            <button onClick={() => handleDeleteSnippet(snippet)}>Delete</button>
           </li>
         ))}
       </ul>
+      {selectedCollection && (
+        <form onSubmit={handleFormSubmit}>
+          <input
+            type="text"
+            value={newSnippetName}
+            onChange={handleInputChange}
+            placeholder="New Snippet Name"
+          />
+          <button type="submit">Add Snippet</button>
+        </form>
+      )}
     </div>
   );
 };
